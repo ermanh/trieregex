d6879eadb21c0d803e60401616ba88e67b0c4645
Herman Leung <leung.hm@gmail.com>
2020-06-19 16:41:05 +0800

    Fill up test_has.py

--- tests/test_has.py
+++ tests/test_has.py
@@ -1,5 +1,3 @@
-# TODO: TO BE COMPLETED
-
 from trieregex.trieregex import TrieRegEx as TRE
 import unittest
 
@@ -9,16 +7,44 @@ class TestHas(unittest.TestCase):
     """
 
     def setUp(self):
-        self.words = ['heart', 'healthy', 'pear', 'peach',
-                      'lark', 'look', 'change']
+        self.words = ['heal', 'health', 'healthy', 'healthier', 'healthiest']
         self.tre = TRE(*self.words)
 
-    def test_has(self):
-        non_existing = ['hear', 'ear', 'each', 'hang', 'ok', 'heal', 'pa']
-        assert (
-            all([self.tre.has(word) for word in self.words]) and
-            not all([self.tre.has(word) for word in non_existing])
+    def test_existing_longest_word(self):
+        self.assertTrue(self.tre.has('healthiest'))
+
+    def test_existing_substring_word(self):
+        self.assertTrue(self.tre.has('health'))
+        self.assertTrue(self.tre.has('heal'))
+
+    def test_nonexisting(self):
+        self.assertFalse(self.tre.has('wound'))
+    
+    def test_nonword_substring_of_existing_word(self):
+        self.assertFalse(self.tre.has('he'))
+
+    def test_nonexisting_after_removed(self):
+        """Also a test of the TrieRegEx.remove() function
+        """
+        self.assertTrue(
+            self.tre.has('healthy'), 
+            "'healthy' must first exist in trie"
         )
+        self.tre.remove('healthy')
+        self.assertFalse(self.tre.has('healthy'))
+
+    def test_existing_after_added(self):
+        """Also a test of the TrieRegEx.add() function
+        """
+        self.assertFalse(
+            self.tre.has('settled'),
+            "'settled' must first NOT exist in trie"
+        )
+        self.tre.add('settled')
+        self.assertTrue(self.tre.has('settled'))
+
+    def test_empty_string(self):
+        self.assertFalse(self.tre.has(''))
 
 
 if __name__ == '__main__':
