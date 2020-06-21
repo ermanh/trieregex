a412122973192ca969a97a9133e5cd523cab48a3
Herman Leung <leung.hm@gmail.com>
2020-06-21 21:47:16 +0800

    Fill up test_regex.py

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -1,5 +1,3 @@
-# TODO: TO BE COMPLETED
- 
 from trieregex.trieregex import TrieRegEx as TRE
 import unittest
 import re
@@ -13,10 +11,18 @@ class TestRegex(unittest.TestCase):
     def setUp(self):
         self.tre = TRE()
         self.words = ['p', 'pe', 'pea', 'pear']
+        self.more_words = [
+            'orange', 'kumquat', 'tangerine', 'mandarin', 'pomelo', 'yuzu', 
+            'grapefruit', 'lemon', 'lime', 'bergamot', 'citron', 'clementine', 
+            'satsuma', 'tangelo', 'mikan', 'tangor',
+            'mint', 'peppermint', 'spearmint', 'basil', 'cilantro',
+            'coriander', 'chives', 'parsley', 'oregano', 'rosemary', 'thyme', 
+            'scallion', 'ginger', 'garlic', 'onion', 'galangal'
+        ]
 
     def findall(self, string: str, boundary: str) -> List[str]:
         """Helper function. The TrieRegEx.regex() function is called here and 
-        the result of regex matching is also returned here
+        the result of regex matching is returned
         """
         pattern = re.compile(f'{boundary}{self.tre.regex()}{boundary}')
         return sorted(pattern.findall(string))
@@ -47,12 +53,71 @@ class TestRegex(unittest.TestCase):
     def test_empty_trie_returns_empty_string_regex(self):
         self.assertEqual(self.tre.regex(), '')
     
+    def test_match_all_words(self):
+        self.tre.add(*self.more_words)
+        found = self.findall(' '.join(sorted(self.more_words)), '\\b')
+        self.assertEqual(found, sorted(self.more_words))
+
+    def test_match_all_words_surrounded_by_spaces(self):
+        words = sorted(self.more_words)
+        self.tre.add(*words)
+        found = re.findall(f"(?<= ){'|'.join(words)}(?= )", ' '.join(words))
+        self.assertEqual(
+            found,
+            words[1:-1],
+            'First and last item in sorted words list should not be matched.'
+        )
+
     def test_added_word_reflected_in_new_regex_call(self):
         self.tre.add(*self.words)
-        self.assertEqual(self.tre.regex(), 'p(?:e(?:ar?)?)?')
-        
+        self.assertEqual(
+            self.tre.regex(), 
+            'p(?:e(?:ar?)?)?', 
+            'Setup for the real test in the next assertEqual'
+        )
         self.tre.add('peak')
         self.assertEqual(self.tre.regex(), 'p(?:e(?:a[kr]?)?)?')
+
+    def test_removed_word_reflected_in_new_regex_call(self):
+        expanded = self.words + ['peak']
+        self.tre.add(*expanded)
+        self.assertEqual(
+            self.tre.regex(), 
+            'p(?:e(?:a[kr]?)?)?',
+            'Setup for the real test in the next assertEqual'
+        )
+        self.tre.remove('peak')
+        self.assertEqual(self.tre.regex(), 'p(?:e(?:ar?)?)?')
+
+    def test_multiple_adding_removing_reflected(self):
+        """This test also checks that the memoizer cache clearing is called
+        in the right places so that .add(), .remove(), and .regex() run
+        correctly as expected
+        """
+        self.tre.add(*self.words)
+        self.assertEqual(
+            self.tre.regex(),
+            'p(?:e(?:ar?)?)?',
+            'Setup for the real test in the next assertEqual'
+        )
+        self.tre.add('peak')
+        self.tre.remove('pe')
+        self.tre.add('river')
+        self.tre.add('rich')
+        self.tre.remove('pea')
+        self.tre.remove('peak')
+        self.assertEqual(
+            self.tre.regex(),
+            '(?:ri(?:ver|ch)|p(?:ear)?)'
+        )
+        self.tre.add('peak')
+        self.tre.remove('peak')
+        self.tre.remove('pear')
+        self.tre.add(*self.words)
+        self.assertEqual(
+            self.tre.regex(),
+            '(?:p(?:e(?:ar?)?)?|ri(?:ver|ch))'
+        )
 
 
 if __name__ == '__main__':
